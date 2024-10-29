b790c774a14004b5031e3157117ded2f1051c52a
Joel Wetzell <jwetzell@yahoo.com>
2024-10-29 07:37:06 -0500

    move logic outside of switch case

--- packages/freed/src/encode.ts
+++ packages/freed/src/encode.ts
@@ -1,33 +1,36 @@
-import { FreeDPosition } from './types';
+import { FreeDPoll, FreeDPosition } from './types';
 import { checksum, positionToFreeDUnits, rotationToFreeDUnits } from './utils';
 
+function positionMessageToBytes(message: FreeDPosition): Uint8Array {
+  const bytes = new Uint8Array(29);
+
+  bytes[0] = 0xd1;
+  bytes[1] = message.id;
+
+  bytes.set(rotationToFreeDUnits(message.pan), 2);
+  bytes.set(rotationToFreeDUnits(message.tilt), 5);
+  bytes.set(rotationToFreeDUnits(message.roll), 8);
+
+  bytes.set(positionToFreeDUnits(message.posX), 11);
+  bytes.set(positionToFreeDUnits(message.posY), 14);
+  bytes.set(positionToFreeDUnits(message.posZ), 17);
+
+  bytes[20] = message.zoom >> 12;
+  bytes[21] = message.zoom >> 8;
+  bytes[22] = message.zoom % 256;
+
+  bytes[23] = message.focus >> 12;
+  bytes[24] = message.focus >> 8;
+  bytes[25] = message.focus % 256;
+
+
+  bytes[28] = checksum(bytes.subarray(0, 28));
+  return bytes;
+}
 export function encode(message: FreeDPosition): Uint8Array {
   switch (message.type) {
     case 0xd1:
-      const view = new Uint8Array(29);
-
-      view[0] = 0xd1;
-      view[1] = message.id;
-
-      view.set(rotationToFreeDUnits(message.pan), 2);
-      view.set(rotationToFreeDUnits(message.tilt), 5);
-      view.set(rotationToFreeDUnits(message.roll), 8);
-
-      view.set(positionToFreeDUnits(message.posX), 11);
-      view.set(positionToFreeDUnits(message.posY), 14);
-      view.set(positionToFreeDUnits(message.posZ), 17);
-
-      view[20] = message.zoom >> 12;
-      view[21] = message.zoom >> 8;
-      view[22] = message.zoom % 256;
-
-      view[23] = message.focus >> 12;
-      view[24] = message.focus >> 8;
-      view[25] = message.focus % 256;
-
-      view[28] = checksum(new Uint8Array(view.buffer, 0, 28));
-      return view;
-
+      return positionMessageToBytes(message);
     default:
       throw new Error('unsupport freeD message type');
   }
